d5a6f696165ace0d1c6916019ac53459cc3b218f
volodymyr_kepsha <kepshavolodymyr1388@gmail.com>
2018-03-27 17:10:12 +0200

    main added

--- cnn.py
+++ cnn.py
@@ -6,8 +6,6 @@ import operator
 import functools
 import os
 
-root_path = '/data_text_form'
-
 
 def get_data(path_r, num_of_pics=None, for_test=None):
     """method for getting numpy matrices of images and one-hot-encode labels for both training and tasting
@@ -59,103 +57,115 @@ def get_data(path_r, num_of_pics=None, for_test=None):
     return train_input, train_label, test_input, test_label
 
 
-train_input, train_label, test_input, test_label = get_data(root_path, num_of_pics=40)
-
-image_hight = train_input.shape[1]
-image_width = train_input.shape[2]
-classes_numb = 10
-
-# -------------------------------------------------------------------------------------------------------------------
-
-X = tf.placeholder(tf.float32, [None, image_hight * image_width * 1], name='X_muliplied')
-
-X_shaped = tf.reshape(X, [-1, image_hight, image_width, 1], name='X_shaped_{}_{}'.format(image_hight, image_width))
-
-Y = tf.placeholder(tf.float32, [None, classes_numb], name="Y_labels")
-
-neurons_in_first_dense = 1024
+def run(root_path):
 
 
-def get_conv_layer(input_data, num_chanels, num_filters, filter_shape, pool_shape, name):
-    """ method for getting a convolutional layer
-        with particular parameters as:
-        num_of_chanels - here it is 1, for grayscale
-        num_of_filters - self explained
-        filter_shape - self explained
-        pool_shape - maxpool shape
-        strides for max pool is 2x2"""
 
-    conv_filter_shape = [filter_shape[0], filter_shape[1], num_chanels, num_filters]
+    train_input, train_label, test_input, test_label = get_data(root_path, num_of_pics=40)
 
-    weights = tf.Variable(tf.truncated_normal(conv_filter_shape, stddev=0.03), name=name + '_W')
+    image_hight = train_input.shape[1]
+    image_width = train_input.shape[2]
+    classes_numb = 10
 
-    biases = tf.Variable(tf.truncated_normal([num_filters], name=name + '_b'))
-
-    strides_conv = [1, 1, 1, 1]
-
-    out_layer = tf.nn.conv2d(input_data, weights, strides=strides_conv, padding='SAME')
-
-    out_layer += biases
-
-    out_layer = tf.nn.relu(out_layer)
-
-    ksize = [1, pool_shape[0], pool_shape[1], 1]
-
-    strides = [1, 2, 2, 1]
-
-    out_layer = tf.nn.max_pool(out_layer, ksize=ksize, strides=strides, padding='SAME')
-
-    return out_layer
+    # -------------------------------------------------------------------------------------------------------------------
 
 
-def get_full_connected(shape=[None, None], prev_layer=None, is_last=False):
-    '''method for getting a fully connected layer layers
-       if last layer flag is false relu is apllied to the layer'''
-    weights = tf.Variable(tf.truncated_normal(shape=shape, stddev=.03, name='wd1'))
-    biases = tf.Variable(tf.truncated_normal(shape=[shape[1]], stddev=.01, name='bd1'))
-    dense_layer = tf.matmul(prev_layer, weights) + biases
-    if is_last:
+
+
+    X = tf.placeholder(tf.float32, [None, image_hight * image_width * 1], name='X_muliplied')
+
+    X_shaped = tf.reshape(X, [-1, image_hight, image_width, 1], name='X_shaped_{}_{}'.format(image_hight, image_width))
+
+    Y = tf.placeholder(tf.float32, [None, classes_numb], name="Y_labels")
+
+    neurons_in_first_dense = 1024
+
+    def get_conv_layer(input_data, num_chanels, num_filters, filter_shape, pool_shape, name):
+        """ method for getting a convolutional layer
+            with particular parameters as:
+            num_of_chanels - here it is 1, for grayscale
+            num_of_filters - self explained
+            filter_shape - self explained
+            pool_shape - maxpool shape
+            strides for max pool is 2x2"""
+
+        conv_filter_shape = [filter_shape[0], filter_shape[1], num_chanels, num_filters]
+
+        weights = tf.Variable(tf.truncated_normal(conv_filter_shape, stddev=0.03), name=name + '_W')
+
+        biases = tf.Variable(tf.truncated_normal([num_filters], name=name + '_b'))
+
+        strides_conv = [1, 1, 1, 1]
+
+        out_layer = tf.nn.conv2d(input_data, weights, strides=strides_conv, padding='SAME')
+
+        out_layer += biases
+
+        out_layer = tf.nn.relu(out_layer)
+
+        ksize = [1, pool_shape[0], pool_shape[1], 1]
+
+        strides = [1, 2, 2, 1]
+
+        out_layer = tf.nn.max_pool(out_layer, ksize=ksize, strides=strides, padding='SAME')
+
+        return out_layer
+
+    def get_full_connected(shape=[None, None], prev_layer=None, is_last=False):
+        '''method for getting a fully connected layer layers
+           if last layer flag is false relu is apllied to the layer'''
+        weights = tf.Variable(tf.truncated_normal(shape=shape, stddev=.03, name='wd1'))
+        biases = tf.Variable(tf.truncated_normal(shape=[shape[1]], stddev=.01, name='bd1'))
+        dense_layer = tf.matmul(prev_layer, weights) + biases
+        if is_last:
+            return dense_layer
+
+        dense_layer = tf.nn.relu(dense_layer)
         return dense_layer
 
-    dense_layer = tf.nn.relu(dense_layer)
-    return dense_layer
+    conv_layer_1 = get_conv_layer(X_shaped, 1, 32, [5, 5], [2, 2], name='layer_1')
+    conv_layer_2 = get_conv_layer(conv_layer_1, 32, 64, [5, 5], [2, 2], name='layer_2')
+
+    flattened_shape = functools.reduce(operator.mul, [i.value for i in conv_layer_2.shape[1:]], 1)
+    flattened = tf.reshape(conv_layer_2, [-1, flattened_shape])
+
+    dense_layer_1 = get_full_connected([flattened_shape, neurons_in_first_dense], prev_layer=flattened, is_last=False)
+    dense_layer_2 = get_full_connected([neurons_in_first_dense, classes_numb], prev_layer=dense_layer_1, is_last=True)
+
+    Y_ = tf.nn.softmax(dense_layer_2)
+    # -------------------------------------------------------------------------------------------------------------------
 
 
-conv_layer_1 = get_conv_layer(X_shaped, 1, 32, [5, 5], [2, 2], name='layer_1')
-conv_layer_2 = get_conv_layer(conv_layer_1, 32, 64, [5, 5], [2, 2], name='layer_2')
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=dense_layer_2, labels=Y))
+    optimiser = tf.train.AdamOptimizer().minimize(cross_entropy)
 
-flattened_shape = functools.reduce(operator.mul, [i.value for i in conv_layer_2.shape[1:]], 1)
-flattened = tf.reshape(conv_layer_2, [-1, flattened_shape])
+    correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    tf.summary.scalar('accuracy', accuracy)
+    merged = tf.summary.merge_all()
 
-dense_layer_1 = get_full_connected([flattened_shape, neurons_in_first_dense], prev_layer=flattened, is_last=False)
-dense_layer_2 = get_full_connected([neurons_in_first_dense, classes_numb], prev_layer=dense_layer_1, is_last=True)
-
-Y_ = tf.nn.softmax(dense_layer_2)
-# -------------------------------------------------------------------------------------------------------------------
-
-
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=dense_layer_2, labels=Y))
-optimiser = tf.train.AdamOptimizer().minimize(cross_entropy)
-
-correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-tf.summary.scalar('accuracy', accuracy)
-merged = tf.summary.merge_all()
-
-init = tf.global_variables_initializer()
-test_acc_list = []
-
-with tf.Session() as sess:
+    init = tf.global_variables_initializer()
     test_acc_list = []
-    sess.run(init)
-    for i in range(50):
-        _, c = sess.run([optimiser, cross_entropy], feed_dict={X_shaped: train_input, Y: train_label})
-        test_acc = sess.run(accuracy, feed_dict={X_shaped: test_input, Y: test_label})
-        print("Epoch:", (i + 1), ' cost: {}'.format(c), " test accuracy: {:.3f}".format(test_acc))
-        test_acc_list.append(test_acc)
-    # for debuging output of the network
-    # print(np.round(sess.run(Y_, feed_dict={X_shaped: train_input, Y: train_label}), 3))
-    # print(np.round(sess.run(Y, feed_dict={X_shaped: train_input, Y: train_label}), 3))
 
-    plt.plot(range(50), test_acc_list)
-    plt.show()
+    with tf.Session() as sess:
+        test_acc_list = []
+        sess.run(init)
+        for i in range(50):
+            _, c = sess.run([optimiser, cross_entropy], feed_dict={X_shaped: train_input, Y: train_label})
+            test_acc = sess.run(accuracy, feed_dict={X_shaped: test_input, Y: test_label})
+            print("Epoch:", (i + 1), ' cost: {}'.format(c), " test accuracy: {:.3f}".format(test_acc))
+            test_acc_list.append(test_acc)
+        # for debuging output of the network
+        # print(np.round(sess.run(Y_, feed_dict={X_shaped: train_input, Y: train_label}), 3))
+        # print(np.round(sess.run(Y, feed_dict={X_shaped: train_input, Y: train_label}), 3))
+
+        plt.plot(range(50), test_acc_list)
+        plt.show()
+
+
+if __name__ == '__main__':
+
+    root_path_ = '/data_text_form'
+
+
+    run(root_path_)
