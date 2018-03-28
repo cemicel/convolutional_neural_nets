797b30b170b46605645b5b7d57ec12a9203f83d5
volodymyr_kepsha <kepshavolodymyr1388@gmail.com>
2018-03-28 08:20:05 +0200

    fixed

--- cnn.py
+++ cnn.py
@@ -7,17 +7,17 @@ import functools
 import os
 
 
-def get_data(path_r, num_of_pics=None, for_test=None):
+def get_data(path_r, numb_of_pictures=None, for_test=None):
     """method for getting numpy matrices of images and one-hot-encode labels for both training and tasting
      Data should be stored in separate files with names i.png (0<=i<=n)
-
     Parameters:
     - num_of_pics - provide particular number of pictures to be extracted,
          if None extracts all.
     - for_test - number of pics for testing. Cannot be bigger than 30% or less that 1
         by default 10%
     """
-    paths = [x[0] for c, x in enumerate(os.walk(os.getcwd() + path_r)) if c > 0]
+
+    paths = [x[0] for c, x in enumerate(os.walk(path_r)) if c > 0]
 
     f = paths[1]
     ath, dirs, files = next(os.walk(f))
@@ -26,16 +26,23 @@ def get_data(path_r, num_of_pics=None, for_test=None):
     test_label = []
     train_input = []
     train_label = []
-    if num_of_pics is None:
+
+    if numb_of_pictures is None and for_test is None:
         num_of_pics = file_count
-    if for_test is None and num_of_pics is None:
         for_test = file_count * len(paths) * .01
-    elif for_test is None and num_of_pics is not None:
-        for_test = num_of_pics * len(paths) * .01
-    elif for_test < num_of_pics * len(paths) * .01:
-        raise ValueError('Not enough for testing')
+    elif numb_of_pictures is not None and for_test is None:
+        for_test = numb_of_pictures * len(paths) * .01
+        num_of_pics = numb_of_pictures
+    elif numb_of_pictures is not None and for_test is not None:
+        for_test = for_test
+        num_of_pics = numb_of_pictures
+
+    if numb_of_pictures < len(paths) or numb_of_pictures < (for_test * 3):
+        raise ValueError('Not enough for training!')
+    elif for_test < numb_of_pictures * len(paths) * .01:
+        raise ValueError('Not enough for testing!')
     elif for_test > file_count * len(paths) * .3:
-        raise ValueError('Too many for testing')
+        raise ValueError('Too many for testing!')
 
     for p_count, path in enumerate(paths):
         one_hot_enc_arr = np.zeros(len(paths))
@@ -54,11 +61,59 @@ def get_data(path_r, num_of_pics=None, for_test=None):
     test_input = np.expand_dims(test_input, -1)
     test_label = np.array(test_label)
 
+    print('Train input shape: {}, train label shape: {}\nTest input shape: {}, test label shape: {}'.
+          format(train_input.shape, (train_label).shape, test_input.shape, (test_label).shape))
+
     return train_input, train_label, test_input, test_label
 
 
-def run(root_path):
-    train_input, train_label, test_input, test_label = get_data(root_path, num_of_pics=40)
+def get_conv_layer(input_data, num_chanels, num_filters, filter_shape, pool_shape, name):
+    """ method for getting a convolutional layer
+        with particular parameters as:
+        num_of_chanels - here it is 1, for grayscale
+        num_of_filters - self explained
+        filter_shape - self explained
+        pool_shape - maxpool shape
+        strides for max pool is 2x2"""
+
+    conv_filter_shape = [filter_shape[0], filter_shape[1], num_chanels, num_filters]
+
+    weights = tf.Variable(tf.truncated_normal(conv_filter_shape, stddev=0.03), name=name + '_W')
+
+    biases = tf.Variable(tf.truncated_normal([num_filters], name=name + '_b'))
+
+    strides_conv = [1, 1, 1, 1]
+
+    out_layer = tf.nn.conv2d(input_data, weights, strides=strides_conv, padding='SAME')
+
+    out_layer += biases
+
+    out_layer = tf.nn.relu(out_layer)
+
+    ksize = [1, pool_shape[0], pool_shape[1], 1]
+
+    strides = [1, 2, 2, 1]
+
+    out_layer = tf.nn.max_pool(out_layer, ksize=ksize, strides=strides, padding='SAME')
+
+    return out_layer
+
+
+def get_full_connected(shape=[None, None], prev_layer=None, is_last=False):
+    '''method for getting a fully connected layer layers
+       if last layer flag is false relu is apllied to the layer'''
+    weights = tf.Variable(tf.truncated_normal(shape=shape, stddev=.03, name='wd1'))
+    biases = tf.Variable(tf.truncated_normal(shape=[shape[1]], stddev=.01, name='bd1'))
+    dense_layer = tf.matmul(prev_layer, weights) + biases
+    if is_last:
+        return dense_layer
+
+    dense_layer = tf.nn.relu(dense_layer)
+    return dense_layer
+
+
+def run(data, train=True, iterations=1, ):
+    train_input, train_label, test_input, test_label = data
 
     image_hight = train_input.shape[1]
     image_width = train_input.shape[2]
@@ -73,49 +128,6 @@ def run(root_path):
     Y = tf.placeholder(tf.float32, [None, classes_numb], name="Y_labels")
 
     neurons_in_first_dense = 1024
-
-    def get_conv_layer(input_data, num_chanels, num_filters, filter_shape, pool_shape, name):
-        """ method for getting a convolutional layer
-            with particular parameters as:
-            num_of_chanels - here it is 1, for grayscale
-            num_of_filters - self explained
-            filter_shape - self explained
-            pool_shape - maxpool shape
-            strides for max pool is 2x2"""
-
-        conv_filter_shape = [filter_shape[0], filter_shape[1], num_chanels, num_filters]
-
-        weights = tf.Variable(tf.truncated_normal(conv_filter_shape, stddev=0.03), name=name + '_W')
-
-        biases = tf.Variable(tf.truncated_normal([num_filters], name=name + '_b'))
-
-        strides_conv = [1, 1, 1, 1]
-
-        out_layer = tf.nn.conv2d(input_data, weights, strides=strides_conv, padding='SAME')
-
-        out_layer += biases
-
-        out_layer = tf.nn.relu(out_layer)
-
-        ksize = [1, pool_shape[0], pool_shape[1], 1]
-
-        strides = [1, 2, 2, 1]
-
-        out_layer = tf.nn.max_pool(out_layer, ksize=ksize, strides=strides, padding='SAME')
-
-        return out_layer
-
-    def get_full_connected(shape=[None, None], prev_layer=None, is_last=False):
-        '''method for getting a fully connected layer layers
-           if last layer flag is false relu is apllied to the layer'''
-        weights = tf.Variable(tf.truncated_normal(shape=shape, stddev=.03, name='wd1'))
-        biases = tf.Variable(tf.truncated_normal(shape=[shape[1]], stddev=.01, name='bd1'))
-        dense_layer = tf.matmul(prev_layer, weights) + biases
-        if is_last:
-            return dense_layer
-
-        dense_layer = tf.nn.relu(dense_layer)
-        return dense_layer
 
     conv_layer_1 = get_conv_layer(X_shaped, 1, 32, [5, 5], [2, 2], name='layer_1')
     conv_layer_2 = get_conv_layer(conv_layer_1, 32, 64, [5, 5], [2, 2], name='layer_2')
@@ -135,28 +147,37 @@ def run(root_path):
     correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     tf.summary.scalar('accuracy', accuracy)
-    merged = tf.summary.merge_all()
+
     # -------------------------------------------------------------------------------------------------------------------
     init = tf.global_variables_initializer()
-    test_acc_list = []
 
+    saver = tf.train.Saver()
     with tf.Session() as sess:
-        test_acc_list = []
-        sess.run(init)
-        for i in range(50):
-            _, c = sess.run([optimiser, cross_entropy], feed_dict={X_shaped: train_input, Y: train_label})
-            test_acc = sess.run(accuracy, feed_dict={X_shaped: test_input, Y: test_label})
-            print("Epoch:", (i + 1), ' cost: {}'.format(c), " test accuracy: {:.3f}".format(test_acc))
-            test_acc_list.append(test_acc)
-        # for debuging output of the network
-        # print(np.round(sess.run(Y_, feed_dict={X_shaped: train_input, Y: train_label}), 3))
-        # print(np.round(sess.run(Y, feed_dict={X_shaped: train_input, Y: train_label}), 3))
-
-        plt.plot(range(50), test_acc_list)
-        plt.show()
+        if train:
+            test_acc_list = []
+            sess.run(init)
+            for i in range(iterations):
+                _, c = sess.run([optimiser, cross_entropy], feed_dict={X_shaped: train_input, Y: train_label})
+                test_acc = sess.run(accuracy, feed_dict={X_shaped: test_input, Y: test_label})
+                print("Epoch:", (i + 1), ' cost: {}'.format(c), " test accuracy: {:.3f}".format(test_acc))
+                test_acc_list.append(test_acc)
+            # for debuging output of the network
+            # print(np.round(sess.run(Y_, feed_dict={X_shaped: train_input, Y: train_label}), 3))
+            # print(np.round(sess.run(Y, feed_dict={X_shaped: train_input, Y: train_label}), 3))
+            save_path = saver.save(sess,
+                                   "/Users/volodymyrkepsha/Documents/Study/Python/Projects/neural_nets/neural_network_tensorflow/save_font/model.ckpt")
+            plt.plot(range(iterations), test_acc_list)
+            plt.show()
+        else:
+            saver.restore(sess,
+                          "/Users/volodymyrkepsha/Documents/Study/Python/Projects/neural_nets/neural_network_tensorflow/save_font/model.ckpt")
+            print(np.round(sess.run(Y, feed_dict={X_shaped: test_input, Y: test_label})))
+            print(np.round(sess.run(Y_, feed_dict={X_shaped: test_input, Y: test_label})))
 
 
 if __name__ == '__main__':
-    root_path_ = '/data_text_form'
+    root_path_ = 'data_text_form/'
 
-    run(root_path_)
+    data_ = get_data(root_path_, numb_of_pictures=10, for_test=3)
+
+    run(data=data_, train=True, iterations=50)
