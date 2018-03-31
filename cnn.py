a70ef83826dc4cb0436a4202569f49ca0dad9adb
volodymyr_kepsha <kepshavolodymyr1388@gmail.com>
2018-03-31 14:02:04 +0200

    -a

--- cnn.py
+++ cnn.py
@@ -5,7 +5,7 @@ import scipy.misc
 import operator
 import functools
 import os
-
+'''
 
 def get_data(path_r, numb_of_pictures=None, for_test=None):
     """method for getting numpy matrices of images and one-hot-encode labels for both training and tasting
@@ -20,12 +20,15 @@ def get_data(path_r, numb_of_pictures=None, for_test=None):
     paths = [x[0] for c, x in enumerate(os.walk(path_r)) if c > 0]
 
     f = paths[1]
-    ath, dirs, files = next(os.walk(f))
+    _, _, files = next(os.walk(f))
+
+
     file_count = len(files) - 1
     test_input = []
     test_label = []
     train_input = []
     train_label = []
+
 
     if numb_of_pictures is None and for_test is None:
         num_of_pics = file_count
@@ -44,16 +47,19 @@ def get_data(path_r, numb_of_pictures=None, for_test=None):
     elif for_test > file_count * len(paths) * .3:
         raise ValueError('Too many for testing!')
 
+
     for p_count, path in enumerate(paths):
         one_hot_enc_arr = np.zeros(len(paths))
         one_hot_enc_arr[p_count] = 1
+
         print(path.split('/')[-1], one_hot_enc_arr)
+
         for pic in range(num_of_pics):
             if pic < for_test:
-                test_input.append(scipy.misc.imread(path + '/{}.png'.format(pic), mode="L"))
+                test_input.append(scipy.misc.imread(path + '/{}.PNG'.format(pic), mode="L"))
                 test_label.append(one_hot_enc_arr)
             else:
-                train_input.append(scipy.misc.imread(path + '/{}.png'.format(pic), mode="L"))
+                train_input.append(scipy.misc.imread(path + '/{}.PNG'.format(pic), mode="L"))
                 train_label.append(one_hot_enc_arr)
 
     train_input = np.expand_dims(train_input, -1)
@@ -65,6 +71,54 @@ def get_data(path_r, numb_of_pictures=None, for_test=None):
     #      format(train_input.shape, (train_label).shape, test_input.shape, (test_label).shape))
 
     return train_input, train_label, test_input, test_label
+'''
+
+
+def get_data_test(path_r, numb_of_pictures=None, for_test=None):
+    """method for getting numpy matrices of images and one-hot-encode labels for both training and tasting
+     Data should be stored in separate files with names i.png (0<=i<=n)
+    Parameters:
+    - num_of_pics - provide particular number of pictures to be extracted,
+         if None extracts all.
+    - for_test - number of pics for testing. Cannot be bigger than 30% or less that 1
+        by default 10%
+    """
+
+    paths = [x[0] for c, x in enumerate(os.walk(path_r)) if c > 0]
+
+    f = paths[1]
+    _, _, files = next(os.walk(f))
+
+
+    file_count = len(files) - 1
+    test_input = []
+    test_label = []
+    train_input = []
+    train_label = []
+
+
+    for p_count, path in enumerate(paths):
+        one_hot_enc_arr = np.zeros(len(paths))
+        one_hot_enc_arr[p_count] = 1
+
+        for pic in range(4):
+            if pic < 2:
+                test_input.append(scipy.misc.imread(path + '/{}.PNG'.format(pic), mode="L"))
+                test_label.append(one_hot_enc_arr)
+            else:
+                train_input.append(scipy.misc.imread(path + '/{}.PNG'.format(pic), mode="L"))
+                train_label.append(one_hot_enc_arr)
+
+    train_input = np.expand_dims(train_input, -1)
+    train_label = np.array(train_label)
+    test_input = np.expand_dims(test_input, -1)
+    test_label = np.array(test_label)
+
+    print('Train input shape: {}, train label shape: {}\nTest input shape: {}, test label shape: {}'.
+          format(train_input.shape, (train_label).shape, test_input.shape, (test_label).shape))
+
+    return train_input, train_label, train_input, train_label
+
 
 
 def get_custom_input(path):
@@ -195,11 +249,15 @@ def run(data, train=True, iterations=1, test_custom=None):
 if __name__ == '__main__':
     root_path_ = 'data_text_form/'
 
-    data_ = get_data(root_path_, numb_of_pictures=90, for_test=10)
+    data_ = get_data_test(root_path_)
+
+
 
     run(data=data_, train=True, iterations=50)
 
-    path_custom_times = '/custom_path_of_image_28x168'
-    image_time = get_custom_input(path=path_custom_times)
-    run(data=data_, train=False, iterations=50, test_custom=[image_time])
+    #path_custom_times = '/Users/volodymyrkepsha/Documents/github/cnn/timeNewRoman.png'
+
+    #image_time = get_custom_input(path=path_custom_times)
+
+    #run(data=data_, train=False, iterations=50, test_custom=[image_time])
 
